b36d6077e475fc1631a35433bc75811e4088fc2d
Emil Sandgren <emil.sandgren.es@gmail.com>
2024-12-20 02:02:14 +0100

    Day 1 complete

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+
+//Setup
+var input = File.OpenText(@".\input.csv");
+
+var rightList = new List<int>();
+var leftList = new List<int>();
+string? row;
+
+while ((row =input.ReadLine()) is not null)
+{
+    var columns = row.Split(" ",StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    rightList.Add(int.Parse(columns[0]));
+    leftList.Add(int.Parse(columns[1]));
+}  
+
+rightList.Sort();
+leftList.Sort();
+
+var distance = 0;
+for (int i = 0; i < rightList.Count; i++)
+{
+    distance += Math.Abs(rightList[i] - leftList[i]);
+}
+
+ Console.WriteLine($"Distance {distance}");
---
+++ Day1/Part2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Setup
+var input = File.OpenText(@".\input.csv");
+
+var leftList = new Dictionary<int, int>();
+var rightList = new Dictionary<int,int>();
+
+string? row;
+
+while ((row = input.ReadLine()) is not null)
+{
+    var columns = row.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    var leftNumber = int.Parse(columns[0]);
+    var rightNumber = int.Parse(columns[1]);
+    if (!leftList.TryAdd(leftNumber, 1)) leftList[leftNumber]++;
+    if (!rightList.TryAdd(rightNumber, 1)) rightList[rightNumber]++;
+}
+
+var distance = 0;
+ foreach (var leftNumber in leftList.Keys)
+{
+    var leftScore = leftList[leftNumber];
+    var rightScore = 0;
+    rightList.TryGetValue(leftNumber, out rightScore);
+
+    distance += leftNumber * leftScore * rightScore;
+}
+
+Console.WriteLine($"Distance: {distance}");
