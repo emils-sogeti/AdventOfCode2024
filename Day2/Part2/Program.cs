3c5ca986aea2218d7b3acb6900bf17aa54690d99
Emil Sandgren <emil@sitescandinavia.se>
2025-01-01 23:35:40 +0100

    Day 2 complete

--- Day2/Part2/Program.cs
+++ Day2/Part2/Program.cs
@@ -1,61 +1,83 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
 using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
 
-Console.WriteLine("Day2, Part 2!");
+Console.WriteLine("Hello, World!");
 
 // Setup
-var input = File.OpenText(@".\input.csv");
+var lines = File.ReadAllLines(@".\input.csv").ToList();
 
-var safe = 0;
-string? report;
+var _safe = 0;
 
-while ((report = input.ReadLine()) is not null)
+int _lastLevelValue;
+int _trend;
+bool _damper;
+var originalLineCount = lines.Count;
+
+for (int i = 0; i < lines.Count - 1; i++)
 {
-    var levels = report.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    var line = lines[i];
 
-    var lastLevelValue = int.Parse(levels[0]);
-    Console.WriteLine($"First level: {lastLevelValue}");
-    var isSafe = true;
-    var trend = -2;
-    var dampener = true;
+    var levels = line.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    _trend = 0;
+    _damper = true;
+    _lastLevelValue = int.Parse(levels[0]);
+    bool isSafe = true;
     foreach (var level in levels[1..])
     {
         var levelValue = int.Parse(level);
-        var diff = levelValue - lastLevelValue;
-        var currentTrend = levelValue.CompareTo(lastLevelValue);
 
-        // Only changes within 1 to 3 
-        if (Math.Abs(diff) < 1 | Math.Abs(diff) > 3) isSafe = false;
-
-        if (trend != -2)
-        {
-            // Trend is shifting
-            if (trend != currentTrend) isSafe = false;
-        }
-
+        isSafe = CheckSafety(levelValue);
         if (!isSafe)
         {
-            if (dampener)
+            if (_damper)
             {
-                // Disregard the faulty level
+                _damper = false;
                 isSafe = true;
-                // Only allowed once
-                dampener = false;
             }
             else
             {
-                // Unsafe
                 break;
             }
         }
-        else
-        {
-            trend = currentTrend;
-            lastLevelValue = levelValue;
-        }
     }
 
-    if (isSafe) safe++;
+    Console.WriteLine($"Level: {line} : {isSafe}");
+
+    if (isSafe)
+    {
+        _safe++;
+    }
+    else if (i < originalLineCount)
+    {
+        var reversedLine = string.Join(" ", levels.Reverse());
+        lines.Add(reversedLine);
+    }
 }
 
-Console.WriteLine($"Safe: {safe}");
+Console.WriteLine($"Safe: {_safe}");
+
+bool CheckSafety(int levelValue)
+{
+    var diff = levelValue - _lastLevelValue;
+    var isSafe = true;
+
+    // Changes allowed between 1 and 3 
+    if (Math.Abs(diff) < 1 | Math.Abs(diff) > 3) isSafe = false;
+
+    var currentTrend = levelValue.CompareTo(_lastLevelValue);
+    if (_trend != 0)
+    {
+        if (_trend != currentTrend) isSafe = false;
+    }
+
+    if (isSafe)
+    {
+        _trend = currentTrend;
+        _lastLevelValue = levelValue;
+    }
+
+    return isSafe;
+}
